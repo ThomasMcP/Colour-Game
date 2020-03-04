f9937db41f559582eae2ef4adda736f75bbc9a6e
Thomas McPhee <mcp.thom@gmail.com>
2020-03-04 12:23:52 +0000

    added click events for squares, base logic for selecting winning square, connected header to winning colour.

--- colourGame.js
+++ colourGame.js
@@ -1,30 +1,39 @@
 const squares = document.getElementsByClassName('square');
 const h1Span = document.querySelector('#rgbDisplay');
 let container = document.querySelector("#container");
+const header = document.querySelector("#header");
+const body = document.querySelector("body");
+let winningColour = "";
+let gameEnd = false;
 
 // GAME SET UP
+// createThreeSquares();
 fillSquares();
 selectedColour();
-createThreeSquares();
 
+for (var i = 0; i < squares.length; i++) {
+  squares[i].addEventListener('click', function() {
+    if (gameEnd === false) {
+      handleColourClick(this);
+    }
+  })
+}
 
-function createThreeSquares() {
-  newSquare = document.createElement('div');
-  newSquare.classList.add('square');
-  newSquare2 = document.createElement('div');
-  newSquare2.classList.add('square');
-  newSquare3 = document.createElement('div');
-  newSquare3.classList.add('square');
-  container.appendChild(newSquare);
-  container.appendChild(newSquare2);
-  container.appendChild(newSquare3);
-  fillSquares();
+function handleColourClick(selectedColor) {
+  if (selectedColor.style["background-color"] === winningColour) {
+    // backgroundColor over ["background-color"] ? Don't know.
+    header.style.backgroundColor = selectedColor.style["background-color"];
+    gameEnd = true;
+  } else {
+    selectedColor.style["background-color"] = body.style["background-color"];
+  }
 }
 
 // Generate Random Colours for All Squares
 function fillSquares() {
   for (let i = 0; i < squares.length; i++) {
     squares[i].style["background-color"] = colourGenerator();
+    console.log(squares[i]);
   }
 }
 
@@ -43,23 +52,8 @@ function selectedColour() {
   // Choose 1 of 6 square Colours
   let randomSquare = squares[Math.floor(Math.random() * Math.floor(squares.length))];
   let colorChosen = randomSquare.style["background-color"];
+  winningColour = colorChosen;
   // Display in h1
   h1Span.textContent = colorChosen.toUpperCase();
-}
 
-
-function threeSquares() {
-
-}
-
-function createThreeSquares() {
-  newSquare = document.createElement('div');
-  newSquare.classList.add('square');
-  newSquare2 = document.createElement('div');
-  newSquare2.classList.add('square');
-  newSquare3 = document.createElement('div');
-  newSquare3.classList.add('square');
-  container.appendChild(newSquare);
-  container.appendChild(newSquare2);
-  container.appendChild(newSquare3);
 }
